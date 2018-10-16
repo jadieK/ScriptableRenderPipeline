594ab03181e8b04848f1b26390cd09b3292c6864
RSlysz <remi@unity3d.com>
2018-10-16 19:07:15 +0200

    Add handles to DensityVolume

--- com.unity.render-pipelines.high-definition/Editor/Lighting/VolumetricLighting/DensityVolumeEditor.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/VolumetricLighting/DensityVolumeEditor.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Experimental.Rendering.HDPipeline;
+using System.Collections.Generic;
+using UnityEditorInternal;
 
 namespace UnityEditor.Experimental.Rendering.HDPipeline
 {
@@ -8,15 +10,42 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
     [CustomEditor(typeof(DensityVolume))]
     class DensityVolumeEditor : Editor
     {
-        static readonly GUIContent s_Size = new GUIContent("Size", "The size of this density volume which is transform's scale independent.");
-        static readonly GUIContent s_AlbedoLabel = new GUIContent("Single Scattering Albedo", "Hue and saturation control the color of the fog (the wavelength of in-scattered light). Value controls scattering (0 = max absorption & no scattering, 1 = no absorption & max scattering).");
-        static readonly GUIContent s_MeanFreePathLabel = new GUIContent("Mean Free Path", "Controls the density, which determines how far you can seen through the fog. It's the distance in meters at which 50% of background light is lost in the fog (due to absorption and out-scattering).");
-        static readonly GUIContent s_VolumeTextureLabel = new GUIContent("Density Mask Texture");
-        static readonly GUIContent s_TextureScrollLabel = new GUIContent("Texture Scroll Speed");
-        static readonly GUIContent s_TextureTileLabel = new GUIContent("Texture Tiling Amount");
-        static readonly GUIContent s_PositiveFadeLabel = new GUIContent("Positive Fade", "Controls the [0, 1] distance from the +X/+Y/+Z face at which a linear fade ends. 0 means no fade, 1 means the fade ends at the opposite face.");
-        static readonly GUIContent s_NegativeFadeLabel = new GUIContent("Negative Fade", "Controls the [0, 1] distance from the -X/-Y/-Z face at which a linear fade ends. 0 means no fade, 1 means the fade ends at the opposite face.");
-        static readonly GUIContent s_InvertFadeLabel = new GUIContent("Invert Fade", "Inverts fade values in such a way that (0 -> 1), (0.5 -> 0.5) and (1 -> 0).");
+        const int maxDisplayedBox = 10;
+
+        const EditMode.SceneViewEditMode EditShape = EditMode.SceneViewEditMode.ReflectionProbeBox;
+        const EditMode.SceneViewEditMode EditBlend = EditMode.SceneViewEditMode.GridBox;
+
+        static class Styles
+        {
+            public static readonly GUIContent[] s_Toolbar_Contents = new GUIContent[]
+            {
+                EditorGUIUtility.IconContent("EditCollider", "|Modify the base shape. (SHIFT+1)"),
+                EditorGUIUtility.IconContent("PreMatCube", "|Modify the influence volume. (SHIFT+2)")
+            };
+
+            public static readonly GUIContent s_Size = new GUIContent("Size", "The size of this density volume which is transform's scale independent.");
+            public static readonly GUIContent s_AlbedoLabel = new GUIContent("Single Scattering Albedo", "Hue and saturation control the color of the fog (the wavelength of in-scattered light). Value controls scattering (0 = max absorption & no scattering, 1 = no absorption & max scattering).");
+            public static readonly GUIContent s_MeanFreePathLabel = new GUIContent("Mean Free Path", "Controls the density, which determines how far you can seen through the fog. It's the distance in meters at which 50% of background light is lost in the fog (due to absorption and out-scattering).");
+            public static readonly GUIContent s_VolumeTextureLabel = new GUIContent("Density Mask Texture");
+            public static readonly GUIContent s_TextureScrollLabel = new GUIContent("Texture Scroll Speed");
+            public static readonly GUIContent s_TextureTileLabel = new GUIContent("Texture Tiling Amount");
+            public static readonly GUIContent s_BlendLabel = new GUIContent("Blend Distance", "Distance from size where the linear fade is done.");
+            public static readonly GUIContent s_InvertFadeLabel = new GUIContent("Invert Fade", "Inverts fade values in such a way that (0 -> 1), (0.5 -> 0.5) and (1 -> 0).");
+            public static readonly GUIContent s_NormalModeContent = new GUIContent("Normal", "Normal parameters mode.");
+            public static readonly GUIContent s_AdvancedModeContent = new GUIContent("Advanced", "Advanced parameters mode.");
+
+            public static readonly Color k_GizmoColorBase = new Color(180 / 255f, 180 / 255f, 180 / 255f, 8 / 255f).gamma;
+
+            public static readonly Color[] k_BaseHandlesColor = new Color[]
+            {
+                new Color(180 / 255f, 180 / 255f, 180 / 255f, 255 / 255f).gamma,
+                new Color(180 / 255f, 180 / 255f, 180 / 255f, 255 / 255f).gamma,
+                new Color(180 / 255f, 180 / 255f, 180 / 255f, 255 / 255f).gamma,
+                new Color(180 / 255f, 180 / 255f, 180 / 255f, 255 / 255f).gamma,
+                new Color(180 / 255f, 180 / 255f, 180 / 255f, 255 / 255f).gamma,
+                new Color(180 / 255f, 180 / 255f, 180 / 255f, 255 / 255f).gamma
+            }; 
+        }
 
         SerializedProperty densityParams;
         SerializedProperty albedo;
@@ -30,7 +59,12 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
 
         SerializedProperty positiveFade;
         SerializedProperty negativeFade;
+        SerializedProperty uniformFade;
+        SerializedProperty advancedFade;
         SerializedProperty invertFade;
+
+        static Dictionary<DensityVolume, HierarchicalBox> shapeBoxes = new Dictionary<DensityVolume, HierarchicalBox>();
+        static Dictionary<DensityVolume, HierarchicalBox> blendBoxes = new Dictionary<DensityVolume, HierarchicalBox>();
 
         void OnEnable()
         {
@@ -45,34 +79,52 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
 
             size = densityParams.FindPropertyRelative("size");
 
+            positiveFade = densityParams.FindPropertyRelative("m_PositiveFade");
+            negativeFade = densityParams.FindPropertyRelative("m_NegativeFade");
+            uniformFade = densityParams.FindPropertyRelative("m_UniformFade");
+            advancedFade = densityParams.FindPropertyRelative("advancedFade");
+            invertFade = densityParams.FindPropertyRelative("invertFade");
 
-            positiveFade  = densityParams.FindPropertyRelative("positiveFade");
-            negativeFade  = densityParams.FindPropertyRelative("negativeFade");
-            invertFade    = densityParams.FindPropertyRelative("invertFade");
+            shapeBoxes.Clear();
+            blendBoxes.Clear();
+            int max = Mathf.Min(targets.Length, maxDisplayedBox);
+            for (int i = 0; i < max; ++i)
+            {
+                var shapeBox = shapeBoxes[targets[i] as DensityVolume] = new HierarchicalBox(Styles.k_GizmoColorBase, Styles.k_BaseHandlesColor);
+                shapeBox.monoHandle = false;
+                blendBoxes[targets[i] as DensityVolume] = new HierarchicalBox(Styles.k_GizmoColorBase, InfluenceVolumeUI.k_HandlesColor, container: shapeBox);
+            }
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            EditorGUILayout.PropertyField(size, s_Size);
-
+            Drawer_AdvancedSwitch();
+            
             EditorGUI.BeginChangeCheck();
             {
-                EditorGUILayout.PropertyField(albedo, s_AlbedoLabel);
-                EditorGUILayout.PropertyField(meanFreePath, s_MeanFreePathLabel);
+                EditorGUILayout.PropertyField(albedo, Styles.s_AlbedoLabel);
+                EditorGUILayout.PropertyField(meanFreePath, Styles.s_MeanFreePathLabel);
 
                 EditorGUILayout.Space();
 
-                EditorGUILayout.PropertyField(positiveFade, s_PositiveFadeLabel);
-                EditorGUILayout.PropertyField(negativeFade, s_NegativeFadeLabel);
-                EditorGUILayout.PropertyField(invertFade, s_InvertFadeLabel);
+                EditorGUILayout.PropertyField(size, Styles.s_Size);
+                if(advancedFade.boolValue)
+                {
+                    CoreEditorUtils.DrawVector6(Styles.s_BlendLabel, positiveFade, negativeFade, Vector3.zero, Vector3.one, InfluenceVolumeUI.k_HandlesColor);
+                }
+                else
+                {
+                    EditorGUILayout.PropertyField(uniformFade, Styles.s_BlendLabel);
+                }
+                EditorGUILayout.PropertyField(invertFade, Styles.s_InvertFadeLabel);
 
                 EditorGUILayout.Space();
 
-                EditorGUILayout.PropertyField(volumeTexture, s_VolumeTextureLabel);
-                EditorGUILayout.PropertyField(textureScroll, s_TextureScrollLabel);
-                EditorGUILayout.PropertyField(textureTile, s_TextureTileLabel);
+                EditorGUILayout.PropertyField(volumeTexture, Styles.s_VolumeTextureLabel);
+                EditorGUILayout.PropertyField(textureScroll, Styles.s_TextureScrollLabel);
+                EditorGUILayout.PropertyField(textureTile, Styles.s_TextureTileLabel);
             }
 
             if (EditorGUI.EndChangeCheck())
@@ -92,6 +144,148 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
             }
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+
+        void Drawer_AdvancedSwitch()
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                bool advanced = advancedFade.boolValue;
+                advanced = !GUILayout.Toggle(!advanced, Styles.s_NormalModeContent, EditorStyles.miniButtonLeft, GUILayout.Width(60f), GUILayout.ExpandWidth(false));
+                advanced = GUILayout.Toggle(advanced, Styles.s_AdvancedModeContent, EditorStyles.miniButtonRight, GUILayout.Width(60f), GUILayout.ExpandWidth(false));
+                foreach (var containedBox in blendBoxes.Values)
+                {
+                    containedBox.monoHandle = !advanced;
+                }
+                if (advancedFade.boolValue ^ advanced)
+                {
+                    advancedFade.boolValue = advanced;
+                }
+            }
+        }
+
+        static Vector3 CenterBlendLocalPosition(DensityVolume densityVolume)
+        {
+            Vector3 size = densityVolume.parameters.size;
+            Vector3 posBlend = densityVolume.parameters.positiveFade;
+            posBlend.x *= size.x;
+            posBlend.y *= size.y;
+            posBlend.z *= size.z;
+            Vector3 negBlend = densityVolume.parameters.negativeFade;
+            negBlend.x *= size.x;
+            negBlend.y *= size.y;
+            negBlend.z *= size.z;
+            Vector3 localPosition = (negBlend - posBlend) * 0.5f;
+            return localPosition;
+        }
+        static Vector3 BlendSize(DensityVolume densityVolume)
+        {
+            Vector3 size = densityVolume.parameters.size;
+            Vector3 blendSize = Vector3.one - densityVolume.parameters.positiveFade - densityVolume.parameters.negativeFade;
+            blendSize.x *= size.x;
+            blendSize.y *= size.y;
+            blendSize.z *= size.z;
+            return blendSize;
+        }
+        
+        [DrawGizmo(GizmoType.Selected|GizmoType.Active)]
+        static void DrawGizmosSelected(DensityVolume densityVolume, GizmoType gizmoType)
+        {
+            using (new Handles.DrawingScope(Matrix4x4.TRS(densityVolume.transform.position, densityVolume.transform.rotation, Vector3.one)))
+            {
+                //// Positive fade box.
+                //Vector3 size = densityVolume.parameters.size;
+                //Handles.color = Color.red;
+                //Vector3 posFade = densityVolume.parameters.positiveFade;
+                //posFade.x *= size.x;
+                //posFade.y *= size.y;
+                //posFade.z *= size.z;
+                //Vector3 posCenter = -0.5f * densityVolume.parameters.positiveFade;
+                //posCenter.x *= size.x;
+                //posCenter.y *= size.y;
+                //posCenter.z *= size.z;
+                //Handles.DrawWireCube(posCenter, size - posFade);
+
+                //// Negative fade box.
+                //Handles.color = Color.blue;
+                //Vector3 negFade = densityVolume.parameters.negativeFade;
+                //negFade.x *= size.x;
+                //negFade.y *= size.y;
+                //negFade.z *= size.z;
+                //Vector3 negCenter = 0.5f * densityVolume.parameters.negativeFade;
+                //negCenter.x *= size.x;
+                //negCenter.y *= size.y;
+                //negCenter.z *= size.z;
+                //Handles.DrawWireCube(negCenter, size - negFade);
+
+                // Blend box
+                HierarchicalBox blendBox = blendBoxes[densityVolume];
+                blendBox.center = CenterBlendLocalPosition(densityVolume);
+                blendBox.size = BlendSize(densityVolume);
+                blendBox.baseColor = densityVolume.parameters.albedo;
+                blendBox.DrawHull(EditMode.editMode == EditShape);
+                
+                // Bounding box.
+                HierarchicalBox shapeBox = shapeBoxes[densityVolume];
+                shapeBox.center = Vector3.zero;
+                shapeBox.size = densityVolume.parameters.size;
+                shapeBox.DrawHull(EditMode.editMode == EditShape);
+
+                //color reseted at end of scope
+            }
+        }
+
+        void OnSceneGUI()
+        {
+            DensityVolume densityVolume = target as DensityVolume;
+            HierarchicalBox shapeBox = shapeBoxes[densityVolume];
+            HierarchicalBox blendBox = blendBoxes[densityVolume];
+            
+            using (new Handles.DrawingScope(Matrix4x4.TRS(densityVolume.transform.position, densityVolume.transform.rotation, Vector3.one)))
+            {
+                //contained must be initialized in all case
+                shapeBox.center = Vector3.zero;
+                shapeBox.size = densityVolume.parameters.size;
+
+                blendBox.monoHandle = !densityVolume.parameters.advancedFade;
+                blendBox.center = CenterBlendLocalPosition(densityVolume);
+                blendBox.size = BlendSize(densityVolume);
+                EditorGUI.BeginChangeCheck();
+                blendBox.DrawHandle();
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(densityVolume, "Change Density Volume Blend");
+
+                    //work in local space to compute the change on positiveFade and negativeFade
+                    Vector3 newCenterBlendLocalPosition = blendBox.center;
+                    Vector3 halfSize = blendBox.size * 0.5f;
+                    Vector3 size = densityVolume.parameters.size;
+                    Vector3 posFade = newCenterBlendLocalPosition + halfSize;
+                    posFade.x = 0.5f - posFade.x / size.x;
+                    posFade.y = 0.5f - posFade.y / size.y;
+                    posFade.z = 0.5f - posFade.z / size.z;
+                    Vector3 negFade = newCenterBlendLocalPosition - halfSize;
+                    negFade.x = 0.5f + negFade.x / size.x;
+                    negFade.y = 0.5f + negFade.y / size.y;
+                    negFade.z = 0.5f + negFade.z / size.z;
+                    densityVolume.parameters.positiveFade = posFade;
+                    densityVolume.parameters.negativeFade = negFade;
+                }
+
+                shapeBox.monoHandle = !densityVolume.parameters.advancedFade;
+                EditorGUI.BeginChangeCheck();
+                shapeBox.DrawHandle();
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObjects(new Object[] { densityVolume, densityVolume.transform }, "ChangeDensity Volume Bounding Box");
+                    densityVolume.transform.position = shapeBox.center;
+                    densityVolume.parameters.size = shapeBox.size;
+                    Vector3 halfSize = shapeBox.size * 0.5f;
+                }
+            }
         }
     }
 }
